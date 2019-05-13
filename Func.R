af706b3f33b0dac2ea8d3a8dc410d605cbb6096b
fansi-sifan <sifan1121@gmail.com>
2019-05-13 09:30:39 -0400

    add function to split-join-merge

--- Func.R
+++ Func.R
@@ -1,7 +1,9 @@
 # Author: Sifan Liu
 # Date: Wed Feb 27 12:16:17 2019
 # --------------
-pkgs <- c('SifanLiu','tidyverse','tigris', "rgdal",'maptools',  "ggmap", "maps", "grid", "gridExtra",'rgeos','broom', 'ggrepel')
+# pkgs <- c('SifanLiu','tidyverse','tigris', "rgdal",'maptools',  "ggmap", "maps", "grid", "gridExtra",'rgeos','broom', 'ggrepel')
+
+pkgs <- c('SifanLiu','tidyverse')
 
 check <- sapply(pkgs,require,warn.conflicts = TRUE,character.only = TRUE)
 if(any(!check)){
@@ -10,37 +12,6 @@ if(any(!check)){
   check <- sapply(pkgs.missing,require,warn.conflicts = TRUE,character.only = TRUE)
 } 
 
-
-# SMEloans plot, peer vs Bham
-opr <- function(df){
-  df %>%
-    group_by(Bham, program, year_range, emp.tot, emp.traded)%>%
-    summarise(amt.tot = sum(as.numeric(amt.tot), na.rm = TRUE))%>%
-    filter(!is.na(year_range))%>%
-    mutate(value = amt.tot/emp.tot*1000)
-}
-
-p_SME <- function(df,pgm){bbplot(data = df%>%filter(program==pgm), 
-                                 aes(x = year_range, y = value/5, fill = Bham))+
-    geom_bar(stat = "identity", position = "dodge")+
-    scale_fill_manual(name = element_blank(), 
-                      values = c("#0070c0", "#ffc000"), 
-                      labels = c( "Peer average", paste(placename,countyname, sep = " /")))+
-    labs(x = NULL, y = NULL)+
-    facet_wrap(~geo, nrow = 1)+
-    theme(legend.position = "bottom")}
-
-
-# bar plot with title and highlights
-
-bar_plot <- function(df,title, HL){
-  bbplot(df %>% filter(!is.na(value)), aes(x = reorder(metro,value), y = value, fill = HL))+
-    geom_bar(stat = "identity")+
-    coord_flip()+
-    labs(title = title, x = NULL,y = NULL)+
-    scale_fill_manual(values = c("#0070c0", "#ffc000"), guide = FALSE)
-}
-
 # SETUP =============================================================
 # assign geocodes for analysis ------------------------
 city_FIPS <- "07000"
@@ -48,40 +19,57 @@ ct_FIPS <- "073"
 msa_FIPS <- "13820"
 st_FIPS <- "01"
 county_FIPS <- paste0(st_FIPS, ct_FIPS)
-
-# tracts within city boundary ------------------------
-xwalk.Bham <- read.csv("source/Bham_city_tracts.csv", colClasses = "character") %>%
-  separate(tract, c("a","b"))%>%
-  mutate(b = replace(b, is.na(b), 0))
-city_tracts <- paste0(padz(xwalk.Bham$a,4), padz(xwalk.Bham$b,2))
-
 # assign names for text display ------------------------
 placename <- "Birmingham"
 countyname <- "Jefferson County, AL"
 metroname <- "Birmingham MSA"
 
-# peers ------------------------
-# peers from clustering result 
+# get tracts within city boundary ------------------------
+# xwalk.Bham <- read.csv("source/Bham_city_tracts.csv", colClasses = "character") %>%
+#   separate(tract, c("a","b"))%>%
+#   mutate(b = replace(b, is.na(b), 0))
+# city_tracts <- paste0(padz(xwalk.Bham$a,4), padz(xwalk.Bham$b,2))
+
+# [Depreciated] peers from clustering result 
 # Peers <- read.csv("source/counties_cluster_all.csv") %>%
 #   mutate(cbsa = as.character(cbsa)) %>%
 #   mutate(stcofips = padz(as.character(stcofips), 5)) %>%
 #   group_by(kmeans) %>%
 #   filter(msa_FIPS %in% cbsa)
 
-peerlist <- c("34980", "47260", "33340", "32820","40060", 
-              "31140", "35380", "15380", "40380","26620",
-              "28140", "17460", "26900", "12940", msa_FIPS)
-Peers <- readxl::read_xlsx('result/13820_Market Assessment.xlsx', sheet = "Peers")%>%
-  filter(cbsa %in% peerlist)
+peers.county <- c("01073","18097","22033","22071","29095",
+                  "36029","36055","39035","47157","51710",
+                  "51760","55079","21111","47037","01089", county_FIPS)
 
-msa_ct_FIPS <- read.csv('V:/Sifan/R/xwalk/county2msa.csv') %>%
-  mutate(fips = paste0(padz(fipsstatecode,2), padz(fipscountycode,3)),
-         COUNTY = trimws(toupper(gsub("County","",countycountyequivalent))))%>%
-  # filter(cbsacode %in% Peers$cbsa)%>%
-  mutate(cbsa = as.character(cbsacode))%>%
-  select(cbsa, FIPS = fips, metro = cbsatitle, county = COUNTY)
+# Peers <- readxl::read_xlsx('result/13820_Market Assessment.xlsx', sheet = "Peers")%>%
+#   filter(cbsa %in% peerlist)
 
-msa_ct_fips <- (msa_ct_FIPS%>%filter(cbsa==msa_FIPS))$FIPS
 
-msa100_FIPS <- as.character((read.csv("V:/Sifan/R/xwalk/top100metros.csv") %>% filter(top100==1))[["GEO.id2"]])
+Peers <- county_cbsa_st %>% filter(code.county %in% peers.county)
+msa_ct_FIPS <- (county_cbsa_st %>% filter(code.cbsa == msa_FIPS))$code.county
+msa100_FIPS <- (metro100 %>% filter(istop100.cbsa))$code.cbsa
 
+
+# function to split df by na value, merge by a different xwalk, bind
+split_join_merge <- function(df.source, df.target,col.filter,col.join){
+  
+  df.matched <- df.source %>% 
+    filter(!is.na(!!col.filter))
+  
+  df.tomatch <- df.source %>% 
+    filter(is.na(!!col.filter))%>%
+    select(-!!col.filter)%>%
+    left_join(df.target, by = col.join)
+  
+  return(bind_rows(df.matched, df.tomatch))
+}
+
+# Visualizations ===================================================
+# bar plot with title and highlights
+bar_plot <- function(df,title, HL){
+  bbplot(df %>% filter(!is.na(value)), aes(x = reorder(metro,value), y = value, fill = HL))+
+    geom_bar(stat = "identity")+
+    coord_flip()+
+    labs(title = title, x = NULL,y = NULL)+
+    scale_fill_manual(values = c("#0070c0", "#ffc000"), guide = FALSE)
+}
